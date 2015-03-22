cca48fa8e6d7aa877f56fa0f028f2a9a355e5b63
AGD <alangdarbyshire@yahoo.co.uk>
2015-03-22 17:18:03 +0000

    Nearly done2

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# DataClean
+
+## SUMMARY
+The R script "run_Analysis.R" can be used process the Human Activity dataset available and described at the following web location :-
+
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+The dataset is downloadable from that location as a zip file.  When the zip file is uncompressed the extracted data will be contained a directory named "UCI HAR Dataset".
+
+The script will produce a summary dataset that is described in more detail in the file "CodeBook.md". 
+
+## RUNNING THE SCRIPT
+
+To run the script in R/Rstudio 
+
+1) Use the R source() command to read the code into the R console session :-
+
+##### > source("run_Analysis.R") 
+
+this assumes the script is in the current working directory, if the script is not in the working directory then you need to provide the appropriate file path 
+
+2) set the R console session working directory to be the "UCI HAR Dataset" directory extracted from the zip file, e.g.
+
+##### > setwd("/home/user/DC/project/UCI HAR Dataset")
+
+3) You can check if the script can locate the data by running the helper function right_place() which returns "TRUE" if the files exist in the expected locations.
+
+##### > right_place()
+#######[1] TRUE
+
+4) Run the "run_Analysis()" script.
+
+##### > run_Analysis()
+
+By default the processed data is written to file "av_output.txt" in the current working directory.  To write to a different file use the "outfile" parameter, e.g.
+
+##### > run_Analysis(outfile = "/home/user/mydata/UCI_output.txt")
+
+5) To read the processed output data into the R session use the helper function "read_averages()".  If the output file is the default location, i.e. in the current working directory and called "av_output.txt" then the fllowing command can be used.
+
+##### > uci_avs <- read_averages()
+
+If the file is in a different location then use the "file" parameter to specify the path to the processed output file, e.g.
+
+##### > uci_avs <- read_averages(file = "/home/user/mydata/UCI_output.txt")
+
+Optionally the run_Analysis script can also write the full combined datasets to an output file (effectively the data as it exists at the end of step 4 of the project assignment).  This requires the use of the "write_combine" parameter, e.g.
+
+##### > run_Analysis(write_combine = "full_merge_data.txt")
+
+## REQUIRED PACKAGES
+The script requires the "plyr" package to be installed.
--- run_Analysis.R
+++ run_Analysis.R
@@ -1,10 +1,29 @@
+library(plyr)
+
+# right_place() is a helper function used to allow the user to determine
+# if the working directory is correctly set for the run_Analysis() function
+
+right_place <- function() {
+	yy <- all(file.exists("activity_labels.txt", "features.txt", "test/subject_test.txt", "test/X_test.txt", "test/y_test.txt", "train/subject_train.txt", "train/X_train.txt", "train/y_train.txt"))
+	yy
+}
+
+# read_averages() is a helper function that can be called to read the processed
+# data written to the output file into a R console session.
+
 read_averages <- function(file = "av_output.txt") {
 	av <- read.table(file)
 	av
 }
 
-average_dataset <- function(ds) {
+# Processes data as described in code book.  Takes means of selected 
+# measurements based on subsets of the data indexed on Subject/Activity.
+
+create_averages_dataset <- function(ds) {
 	nr <- 1
+
+	# Create the Subject/Activity index data frame
+
 	pkey <- unique(ds$Subject)
 	skey <- unique(ds$Activity)
 	maxrow <- length(pkey) * length(skey)
@@ -20,20 +39,36 @@ average_dataset <- function(ds) {
 		}
 	}
 	av <- data.frame(Subject=pidx, Activity=sidx)
+	
+	# Generate the count of observations of each Subject/Activity Pair
+
+	nav <- names(av)
+	av <- ddply(av, c(.(Subject), .(Activity)), nrow)
+
+	nav <- c(nav, "Count")
+	names(av) <- nav
+
+	# Generate the summary data from measurements of each subject and activity
+
 	dsname <- names(ds)
 	dsn <- length(dsname)
 	for (i in dsname[3:dsn]) {
 		eval(parse(text = paste0("ret <- ddply(ds, c(.(Subject), .(Activity)), summarize, ", i, "=mean(", i, ", na.rm=TRUE))")))
 		av <- merge(av, ret, all=TRUE)
 	}
+
+	# Order the resulting dataset based on Subject ID and Activity
+	av <- av[order(av$Subject, av$Activity),]
 	av
 }
 
-
+# Reads Activity Labels
 
 get_act_labels <- function(file = "./activity_labels.txt") {
 	al <- read.table(file, stringsAsFactors=FALSE)
 }
+
+# Helper function to retrieve the indices of the mean/standard deviation of acceleration/angular velocity measurements.  Modify the field names to those described in the codebook.
 
 get_data_indices_labels <- function(file = "./features.txt") {
 	idl <- read.table(file)
@@ -42,6 +77,8 @@ get_data_indices_labels <- function(file = "./features.txt") {
 	idl$V2 <- gsub("-", "_", idl$V2)
 	idl
 }
+
+# Read subset of the UCI dataset from a given directory location
 
 read_dataset <- function(dir, idl, nrows=-1) {
 	save_dir <- getwd()
@@ -63,16 +100,54 @@ read_dataset <- function(dir, idl, nrows=-1) {
 	xx
 }
 
+# run_Analysis() is the function required to executed by the user
+# to process the UCI data
+#
+# By default, it will read all the data in the test and training datasets
+# The parameter "nrows" can be used to read a given number of rows 
+# in each dataset. 
+#
+# The processed data will be written to a text file called 
+# "av_output.txt" unless the parameter "outfile" is used to specify 
+# the output file name.
+#
+# The parameter "write_combine" can be used to write the merged UCI dataset 
+# with descriptive labels and field names (these are the same as in the 
+# final output file). My default this file is not written.  The value of 
+# write_combine should be a character string containing the filename/path 
+# to write the combined data into.
 
-run_Analysis <- function(nrows = -1, outfile="av_output.txt") {
-	save_dir <- getwd()
+
+run_Analysis <- function(nrows = -1, outfile="av_output.txt", write_combine=NULL) {
+
+# Read the indices of the each field and field name associated with each measurement that is a mean() or standard deviation of a measured acceleration or angular velocity (see codebook for more details)
+
 	dil <- get_data_indices_labels()
+
+# Read the Activity Labels 
 	act_lab <- get_act_labels()
+
+# Read the UCI training dataset & apply field names
 	d1 <- read_dataset("train", dil, nrows = nrows)
+
+# Read the UCI test dataset & apply field names
 	d2 <- read_dataset("test", dil, nrows = nrows)
+
+# Merge the two datasets
 	dcom <- rbind(d1, d2)
+
+# Apply the Activity labels to the dataset
 	dcom$Activity <- factor(dcom$Activity, levels = act_lab$V1, labels = act_lab$V2)
-	setwd(save_dir)
-	av <- average_dataset(dcom)
+
+# Check if user wants combined dataset written out
+
+	if (! is.null(write_combine)) {
+		write.table(dcom, write_combine)
+	}
+
+# Process the data to produce the summary dataset 
+	av <- create_averages_dataset(dcom)
+
+# Write the data to the output file
 	write.table(av, file = outfile)
 }
